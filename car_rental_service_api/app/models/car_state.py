df43533784cf186f6ae11a89ddd17b84c43638ac
GraceOnwere <graceonwere.ng@gmail.com>
2026-08-25 16:07:19 +0100

    added data models

---
+++ car_rental_service_api/app/models/car_state.py
@@ -0,0 +1,5 @@
+from enum import Enum ,auto
+
+class CarState(Enum):
+    AVAILABLE = auto()
+    NOT_AVAILABLE = auto()
---
+++ car_rental_service_api/app/models/fleet_manager.py
@@ -0,0 +1,7 @@
+from app.models import role
+from app.models.role import Role
+from app.models.user import User
+
+class FleetManager(User):
+    __role : Role = Role.FLEET_MANAGER
+
